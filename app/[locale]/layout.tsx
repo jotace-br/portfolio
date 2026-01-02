c12973dd963e4dcf12e34d3fb816642097c9fcf9
Júlio César <jc10ferreira@gmail.com>
2026-01-02 20:33:36 -0300

    fix: add analytics and speed insights components to RootLayout

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -4,8 +4,6 @@ import { Header } from '@/components/sections/header';
 import { routing } from '@/i18n/routing';
 import { ThemeProvider } from '@/providers/theme-provider';
 import { getBaseUrl } from '@/utils/url';
-import { Analytics } from '@vercel/analytics/next';
-import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata } from 'next';
 import { hasLocale, NextIntlClientProvider } from 'next-intl';
 import { getMessages, setRequestLocale } from 'next-intl/server';
@@ -160,9 +158,6 @@ export default async function LocaleLayout({
 						<Footer />
 					</ThemeProvider>
 				</NextIntlClientProvider>
-
-				<Analytics />
-				<SpeedInsights />
 			</body>
 		</html>
 	);
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,14 @@
+import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { ReactNode } from 'react';
 import './globals.css';
 
-export default function RootLayout({
-	children,
-}: {
-	children: ReactNode;
-}) {
-	return children;
+export default function RootLayout({ children }: { children: ReactNode }) {
+	return (
+		<>
+			{children}
+			<Analytics />
+			<SpeedInsights />
+		</>
+	);
 }
